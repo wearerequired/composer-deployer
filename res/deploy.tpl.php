fbcf98cea3810e4c5e854bf3f51b44bff85f70a9
Dominik Schilling <dominikschilling+git@gmail.com>
2021-12-13 12:28:27 +0100

    Introduce `wp:post_rollout` task to run custom WP-CLI commands

--- res/deploy.tpl.php
+++ res/deploy.tpl.php
@@ -149,6 +149,30 @@ task(
 	}
 );
 
+desc( 'Run WP-CLI commands before finishing the deployment' );
+task(
+	'wp:post_rollout',
+	function (): void {
+		if ( ! get( 'wordpress' ) || ! has( 'post_rollout_wp_cli_commands' ) ) {
+			return;
+		}
+
+		$commands = get( 'post_rollout_wp_cli_commands' );
+		if ( ! \is_array( $commands ) ) {
+			$commands = [ $commands ];
+		}
+
+		within(
+			'{{release_path}}',
+			function () use ( $commands ): void {
+				foreach ( $commands as $command ) {
+					run( "{{bin/wp}} {$command}" );
+				}
+			}
+		);
+	}
+);
+
 desc( 'Deploy your project' );
 task(
 	'deploy',
@@ -167,6 +191,7 @@ task(
 		'deploy:symlink',
 		'wp:opcache_clear',
 		'wp:upgrade_db',
+		'wp:post_rollout',
 		'deploy:unlock',
 		'cleanup',
 		'success',
